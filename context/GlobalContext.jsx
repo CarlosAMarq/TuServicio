a3f6c3a4dd0b1437b64a21688e6b8b6fcc3a5287
Dayniel Lazaro <mdayniel@gmail.com>
2024-03-02 04:30:25 -0500

    adasd

--- context/GlobalContext.jsx
+++ context/GlobalContext.jsx
@@ -1,9 +1,10 @@
 import React, { createContext, useState } from "react";
+import accounts from '../mocks/accounts.json'
 
 export const GlobalContext = createContext();
 
 export default function GlobalProvider({ children }) {
-  const [currentUser, setCurrentUser] = useState({username: "Invitado"});
+  const [currentUser, setCurrentUser] = useState(accounts.testUser);
 
   return (
     <GlobalContext.Provider value={{ currentUser, setCurrentUser }}>
